013d0c457fd1aa888f0de62acbf4d4bb9f996ee7
apsgangwar <apoorvasinghgangwar@gmail.com>
2022-07-28 01:21:14 +0530

    CSS changes

--- chat_front/project/static/js/pyquiz_sockets.js
+++ chat_front/project/static/js/pyquiz_sockets.js
@@ -1,68 +1,135 @@
 var namespace = '/chat';
 var socket = io.connect(service_host + namespace);
 
-var user_id = "";
-var username = "";
+var user_id = '';
+var username = sessionStorage.getItem('current_user');
+if (!username || username === 'null' || username === 'undefined') {
+  username = '';
+}
+var all_msg_data = {};
+
+var randomNicknames = [
+  'hopeful_bardeen',
+  'determined_mestorf',
+  'wizardly_archimedes',
+  'hungry_jennings',
+  'calm_uncle',
+  'warm_salad',
+  'clever_raman',
+  'happy_einstein',
+  'silly_bose',
+  'kickass_mayer',
+];
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function send_msg(response) {
-    var msg = {"message": response, "username": username};
-    console.log("Enviado mensaje: ");
-    console.log(msg);
-    socket.emit('send_msg', msg);
+  var msg = { message: response, username: username };
+  console.log('Enviado mensaje: ');
+  console.log(msg);
+  socket.emit('send_msg', msg);
+}
+
+function putMsgHtml() {
+  console.log({ all_msg_data });
+  var userRow = '';
+  var username_sender = '';
+  var msgs = '';
+
+  Object.values(all_msg_data).forEach((d) => {
+    if (d.user_id === user_id || d.username === username) {
+      userRow = 'user-row';
+      username_sender = '';
+    } else {
+      userRow = '';
+      username_sender = d.username;
+    }
+
+    msgs =
+      msgs +
+      `
+      <div class="msg-row ${userRow}" >
+        <div class="sender-name ${
+          userRow ? 'display-none' : ''
+        }">${username_sender}</div>
+        <div class="msg-info ${userRow ? 'msg-info-right' : ''}">
+          ${d.message}
+        </div>
+      </div>
+      `;
+  });
+
+  $('#msgs').html(msgs);
+  $('main').scrollTop($('main')[0].scrollHeight);
 }
 
 $(document).ready(function () {
-    socket.on('connect', function () {
-        console.log("connected");
-        $("#start-off").css("display", "none");
-        $("#start-on").css("display", "block");
-        username = prompt("Introduce tu nick", "Harry Potter");
-        socket.emit('log-in', {"username": username});
+  $('#username').text(username);
 
-    });
+  // $('#msg').text('Hola dchsxnkad!!');
+  // $('#modal1').modal('open');
+  // $('#start-off').css('display', 'none');
+  // $('#start-on').css('display', 'block');
+  // putMsgHtml();
 
-    socket.on('users_connected', function (lenUsers) {
-        $('#user-cnt').text(lenUsers);
-        console.log("users connected: " + lenUsers);
-    });
-    socket.on('msgs', function (data) {
-        console.log("llegó mensaje:");
-        console.log(data);
-        var position = "";
-        var username_sender = "";
-        var color = "";
-        if (data.user_id === user_id || data.username === username) {
-            position = "right";
-            color = "light-green lighten-4";
-        } else {
-            color = "grey lighten-5";
-            username_sender = '<div>' + data.username + '</div>';
-        }
-        var msg = '<div class="row ' + color + '">' +
-            username_sender +
-            '<div class="col  s12 m12" ><p class="' + position + '">' +
-            data.message +
-            '</p></div></div>';
-        $('#msgs').append(msg);
+  socket.on('connect', function () {
+    console.log('connected');
+    $('#start-off').css('display', 'none');
+    $('#start-on').css('display', 'block');
 
-        $('main').scrollTop($('main')[0].scrollHeight);
-    });
-    $("#send_msg").submit(function (e) {
-        e.preventDefault();
-        send_msg($("#msg_input").val());
-    });
-    socket.on('message', function (data) {
-        console.log("Message recived");
-        console.log(data);
-        if (data.welcome) {
-            $('#msg').text(data.welcome + "!!");
-        } else {
-            $('#msg').text(data.message);
-        }
-        if (data.user_id) {
-            user_id = data.user_id;
-        }
+    var current_user = sessionStorage.getItem('current_user');
+    if (
+      !current_user ||
+      current_user === 'null' ||
+      current_user === 'undefined'
+    ) {
+      var randomNick = randomNicknames[randomInteger(0, 9)];
+      username = prompt('Enter your nickname:', randomNick);
+      if (!username || username === 'null' || username === 'undefined') {
+        username = randomNick;
+      }
+    } else {
+      username = current_user;
+    }
+
+    console.log({ current_user, username });
+    $('#username').text(username);
+    socket.emit('log-in', { username: username });
+    sessionStorage.setItem('current_user', username);
+  });
+
+  socket.on('users_connected', function (lenUsers) {
+    $('#user-cnt').text(lenUsers);
+    console.log('users connected: ' + lenUsers);
+  });
+
+  socket.on('msgs', function (data) {
+    all_msg_data[data.id] = data;
+    putMsgHtml();
+  });
+
+  $('#send_msg').submit(function (e) {
+    e.preventDefault();
+    send_msg($('#msg_input').val());
+  });
+
+  socket.on('message', function (data) {
+    console.log('Message recived');
+    console.log(data);
+    if (data.welcome) {
+      var current_user = sessionStorage.getItem('current_user');
+      if (`Hola ${current_user}` !== data.welcome) {
+        $('#msg').text(data.welcome + '!!');
         $('#modal1').modal('open');
-    });
-
+      }
+    } else {
+      $('#msg').text(data.message);
+      $('#modal1').modal('open');
+    }
+    if (data.user_id) {
+      user_id = data.user_id;
+    }
+  });
 });
